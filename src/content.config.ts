547ee732ae5d2d6f1606c26ba9ad09bcc2bfd015
Chechu <contact@chechu.dev>
2025-04-09 21:55:11 +0200

    feat: add content configuration for project showcase

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({ 
+    loader: glob({
+        pattern: ["**/*.mdx"],
+        base: "./src/content/projects"
+    }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),     
+        thumbnail: z.string(),   
+    })
+});
+
+export const collections = { projects };
