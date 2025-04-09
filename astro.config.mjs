e23ba7dae940201500a70e87883c5c4e66cb3c64
Chechu <contact@chechu.dev>
2025-04-09 21:46:06 +0200

    feat: use tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
